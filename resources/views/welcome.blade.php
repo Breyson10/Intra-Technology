20e71c1182ff9d82ab5165a1c7785869bddd8c69
Breyson Mosquera <breysonjose107@gmail.com>
2021-01-21 21:56:38 +0100

    Commit-one

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,355 @@
+<!DOCTYPE php>
+<html lang="es">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>IntraTechnology</title>
+  <meta content="" name="descriptison">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="{{asset('assets/img/IconIntra_Color.png')}}" rel="icon">
+  <link href="assets/img/apple-touch-icon-intra.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link
+    href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Montserrat:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i"
+    rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/icofont/icofont.min.css" rel="stylesheet">
+  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="assets/vendor/venobox/venobox.css" rel="stylesheet">
+  <link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+  <link href="assets/vendor/owl.carousel/assets/owl.carousel.min.css" rel="stylesheet">
+  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+  <script src="https://kit.fontawesome.com/b3c4c4d63d.js" crossorigin="anonymous"></script>
+
+
+  <!-- Template Main CSS File -->
+  <link href="assets/css/style.css" rel="stylesheet">
+
+</head>
+
+<body>
+
+  <!-- ======= Header ======= -->
+  <header id="header" class="fixed-top d-flex align-items-center">
+    <div class="container d-flex align-items-center">
+
+      <div class="logo mr-auto">
+        <h1 class="text-light"><a href="index.php"><span>IntraTechnology</span></a></h1>
+        <!-- Uncomment below if you prefer to use an image logo -->
+        <!-- <a href="index.html"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
+      </div>
+
+      <nav class="nav-menu d-none d-lg-block">
+        <ul>
+          <li class="   "><a href="index.php">Inicio</a></li>
+          <li><a href="Demos.php">Demos</a></li>
+          <li class="drop-down"><a href="">Servicios</a>
+            <ul>
+              <li class="drop-down"><a href="#">Diseños</a>
+                <ul>
+                  <li><a href="Web.php">UX</a></li>
+                  <li><a href="Web.php">UI</a></li>
+                  <li><a href="Web.php">LANDING PAGE</a></li>
+                </ul>
+              </li>
+              <li class="drop-down"><a href="#">Desarrollos</a>
+                <ul>
+                  <li><a href="VideoJuegos.html">VIDEOJUEGOS</a></li>
+                  <li><a href="Web.php">WEB</a></li>
+                  <li><a href="Apps.php">APP ANDROID</a></li>
+                  <li><a href="Apps.php">APP IOS</a></li>
+                  <li><a href="Apps.php">APP HIBRIDAS</a></li>
+                </ul>
+              </li>
+              <li class="drop-down"><a href="#">Inteligencia Artificial</a>
+                <ul>
+                  <li><a href="#">PRÓXIMAMENTE</a></li>
+                </ul>
+              </li>
+              <li class="drop-down"><a href="#">3D</a>
+                <ul>
+                  <li><a href="RealidadAR.php">REALIDAD AUMENTADA</a></li>
+                  <li><a href="RealidadVR.php">REALIDAD VIRTUAL</a></li>
+
+
+                </ul>
+              </li>
+            </ul>
+
+          </li>
+          <li><a href="#contact">Contacto</a></li>
+
+        </ul>
+      </nav><!-- .nav-menu -->
+
+    </div>
+  </header><!-- End Header -->
+
+  <!-- ======= Hero Section ======= -->
+  <section id="hero">
+
+    <div class="container">
+      <div class="row">
+          <div data-aos="zoom-out">
+            <h1>Doing More to Bring Your <br> Business to Tech Success</h1>
+            <div class="text-center text-lg-left">
+              <a href="#about" class="btn-get-started scrollto">más info</a>
+            </div>
+          </div>
+
+      </div>
+    </div>
+
+    <svg class="hero-waves" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
+      viewBox="0 24 150 28 " preserveAspectRatio="none">
+      <defs>
+        <path id="wave-path" d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z">
+      </defs>
+      <g class="wave1">
+        <use xlink:href="#wave-path" x="50" y="3" fill="rgba(255,255,255, .1)">
+      </g>
+      <g class="wave2">
+        <use xlink:href="#wave-path" x="50" y="0" fill="rgba(255,255,255, .2)">
+      </g>
+      <g class="wave3">
+        <use xlink:href="#wave-path" x="50" y="9" fill="#fff">
+      </g>
+    </svg>
+
+  </section><!-- End Hero -->
+
+  <main id="main">
+
+    <!-- ======= About Section ======= -->
+    <section id="about" class="about">
+      <div class="container-fluid">
+
+        <div class="row">
+          <!-- <div class="col-xl-5 col-lg-6 video-box d-flex justify-content-center align-items-stretch"
+            data-aos="fade-right">
+            <a href="https://www.youtube.com/watch?v=jDDaplaOz7Q" class="venobox play-btn mb-4" data-vbtype="video"
+              data-autoplay="true"></a></div> -->
+              <div class="col-md-4" data-aos="fade-right">
+                <img src="assets/img/about.png" class="img-fluid" alt="">
+              </div>
+          <div
+            class="col-xl-7 col-lg-6 icon-boxes d-flex flex-column align-items-stretch justify-content-center py-5 px-lg-5"
+            data-aos="fade-left">
+            <h3>Desarrollos a tu alcanse</h3>
+            <p>La 4 Revolución Industrial ya esta aqui, da el paso y impulsa tu negocio ó empresa con las nuevas tecnologias.</p>
+
+
+            <div class="icon-box" data-aos="zoom-in" data-aos-delay="100">
+              <div class="icon"><i class="fas fa-microchip"></i></div>
+              <h4 class="title"><a href="">Innovación Tecnológica</a></h4>
+              <p class="description">Comienza Aplicar la innovación a las diferentes áreas de la empresa y potencializa
+                tus procesos.</p>
+            </div>
+
+            <div class="icon-box" data-aos="zoom-in" data-aos-delay="200">
+              <div class="icon"><i class="fas fa-vr-cardboard"></i></div>
+              <h4 class="title"><a href="">Experiencia Inmersiva</a></h4>
+              <p class="description">El cliente interactuará con la marca de una manera diferente y única, generando un
+                fuerte engagement.</p>
+            </div>
+
+            <div class="icon-box" data-aos="zoom-in" data-aos-delay="300">
+              <div class="icon"><i class="far fa-money-bill-alt"></i></div>
+              <h4 class="title"><a href="">Ahorro de dinero</a></h4>
+              <p class="description">Disminución de la inversión destinada al marketing convensional y ahorro en tus
+                procesos automatizados</p>
+            </div>
+
+          </div>
+        </div>
+
+      </div>
+    </section><!-- End About Section -->
+
+    <!-- ======= Features Section ======= -->
+    <section id="features" class="features">
+      <div class="container">
+
+        <div class="section-title" data-aos="fade-up">
+          <h2>Servicios</h2>
+          <p>Carasteristicas de los Servicios</p>
+        </div>
+
+        <div class="row" data-aos="fade-left">
+          <div class="col-lg-3 col-md-4">
+            <div class="icon-box" data-aos="zoom-in" data-aos-delay="50">
+              <i class="fas fa-chalkboard-teacher" style="color: #ffbb2c;"></i>
+              <h3><a href="">Educación</a></h3>
+            </div>
+          </div>
+          <div class="col-lg-3 col-md-4 mt-4 mt-md-0">
+            <div class="icon-box" data-aos="zoom-in" data-aos-delay="100">
+              <i class="fas fa-theater-masks" style="color: #5578ff;"></i>
+              <h3><a href="">Entretenimiento</a></h3>
+            </div>
+          </div>
+          <div class="col-lg-3 col-md-4 mt-4 mt-md-0">
+            <div class="icon-box" data-aos="zoom-in" data-aos-delay="150">
+              <i class="fas fa-robot" style="color: #e80368;"></i>
+              <h3><a href="">Industria 4.0</a></h3>
+            </div>
+          </div>
+          <div class="col-lg-3 col-md-4 mt-4 mt-lg-0">
+            <div class="icon-box" data-aos="zoom-in" data-aos-delay="200">
+              <i class="fas fa-book-reader" style="color: #e361ff;"></i>
+              <h3><a href="">Catalogos Virtuales</a></h3>
+            </div>
+          </div>
+          <div class="col-lg-3 col-md-4 mt-4">
+            <div class="icon-box" data-aos="zoom-in" data-aos-delay="250">
+              <i class="fas fa-film" style="color: #47aeff;"></i>
+              <h3><a href="">Entornos Inmersivos</a></h3>
+            </div>
+          </div>
+          <div class="col-lg-3 col-md-4 mt-4">
+            <div class="icon-box" data-aos="zoom-in" data-aos-delay="300">
+              <i class="fas fa-assistive-listening-systems" style="color: #ffa76e;"></i>
+              <h3><a href="">Ar assist</a></h3>
+            </div>
+          </div>
+          <div class="col-lg-3 col-md-4 mt-4">
+            <div class="icon-box" data-aos="zoom-in" data-aos-delay="350">
+              <i class="fas fa-chalkboard" style="color: #11dbcf;"></i>
+              <h3><a href="">Formacion Virtual</a></h3>
+            </div>
+          </div>
+          <div class="col-lg-3 col-md-4 mt-4">
+            <div class="icon-box" data-aos="zoom-in" data-aos-delay="400">
+              <i class="fas fa-fighter-jet" style="color: #4233ff;"></i>
+              <h3><a href="">Formacion en RV y AR</a></h3>
+            </div>
+          </div>
+          <div class="col-lg-3 col-md-4 mt-4">
+            <div class="icon-box" data-aos="zoom-in" data-aos-delay="450">
+              <i class="fas fa-photo-video" style="color: #b2904f;"></i>
+              <h3><a href="">Simulaciónes</a></h3>
+            </div>
+          </div>
+          <div class="col-lg-3 col-md-4 mt-4">
+            <div class="icon-box" data-aos="zoom-in" data-aos-delay="500">
+              <i class="fas fa-feather-alt" style="color: #b20969;"></i>
+              <h3><a href="index copy.html">Diseños UX y UI</a></h3>
+            </div>
+          </div>
+          <div class="col-lg-3 col-md-4 mt-4">
+            <div class="icon-box" data-aos="zoom-in" data-aos-delay="550">
+              <i class="fas fa-gamepad" style="color: #ff5828;"></i>
+              <h3><a href="">Videojuegos</a></h3>
+            </div>
+          </div>
+          <div class="col-lg-3 col-md-4 mt-4">
+            <div class="icon-box" data-aos="zoom-in" data-aos-delay="600">
+              <i class="fas fa-dice-d20" style="color: #29cc61;"></i>
+              <h3><a href="">Inteligencia Artificial</a></h3>
+            </div>
+          </div>
+        </div>
+
+      </div>
+  </main><!-- End #main -->
+
+  <!-- ======= Footer ======= -->
+  <footer id="footer">
+    <div class="footer-top">
+      <div class="container">
+        <div class="row">
+
+          <div class="col-lg-4 col-md-6">
+            <div class="footer-info">
+              <h3>IntraTechnology</h3>
+              <p class="pb-3"><em>Tu lo Sueñas ,Nosotros lo Desarrollamos;
+                  Desarrollos a tu Alcanse.
+                </em></p>
+              <p>
+                Calle 58 # 55 - 66. <br>
+                Barranquilla, Colombia<br><br>
+                <strong>Teléfono:</strong> +57 3117008071<br>
+                <strong>Correo:</strong> Ventas@IntraTechnology.co<br>
+              </p>
+              <div class="social-links mt-3">
+                <a href="https://www.facebook.com/IntraTechnologyCo/" class="facebook"><i
+                    class="bx bxl-facebook"></i></a>
+                <a href="https://www.instagram.com/intratechnology_/" class="instagram"><i
+                    class="bx bxl-instagram"></i></a>
+                <a href="https://www.behance.net/intratechnology" class="Behance"><i class="bx bxl-behance"></i></a>
+                <a href="https://www.linkedin.com/company/64745590/admin/" class="linkedin"><i
+                    class="bx bxl-linkedin"></i></a>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-2 col-md-6 footer-links">
+            <h4>Enlaces</h4>
+            <ul>
+              <li><i class="bx bx-chevron-right"></i> <a href="index.php">Inicio</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="Demos.php">Demos</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="index.php">Contacto</a></li>
+            </ul>
+          </div>
+
+          <div class="col-lg-2 col-md-6 footer-links">
+            <h4>Diseños y Desarrollos</h4>
+            <ul>
+              <li><i class="bx bx-chevron-right"></i> <a href="Web.php">UX y UI</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="Web.php">Landing Page</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="VideoJuegos.php">Videojuegos</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="Web.php">Web</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="Apps.php">Android & IOS</a></li>
+            </ul>
+          </div>
+
+          <div class="col-lg-2 col-md-6 footer-links">
+            <h4>3D</h4>
+            <ul>
+              <li><i class="bx bx-chevron-right"></i> <a href="RealidadAR.php">Realidad Aumentada</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="RealidadVR.php">Realidad Virtual</a></li>
+
+            </ul>
+
+          </div>
+
+        </div>
+      </div>
+    </div>
+
+    <div class="container">
+      <div class="copyright">
+        &copy; Copyright <strong><span>IntraTechnology</span></strong>. Todos los derechos reservados
+      </div>
+      <div class="credits">
+      </div>
+    </div>
+  </footer><!-- Final Footer -->
+
+  <a href="#" class="back-to-top"><i class="icofont-simple-up"></i></a>
+  <div id="preloader"></div>
+
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/jquery/jquery.min.js"></script>
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/jquery.easing/jquery.easing.min.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+  <script src="assets/vendor/venobox/venobox.min.js"></script>
+  <script src="assets/vendor/waypoints/jquery.waypoints.min.js"></script>
+  <script src="assets/vendor/counterup/counterup.min.js"></script>
+  <script src="assets/vendor/owl.carousel/owl.carousel.min.js"></script>
+  <script src="assets/vendor/aos/aos.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
